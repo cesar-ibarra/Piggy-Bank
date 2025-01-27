d824a7d95d73639caf5ac96dc8d42a0c717a6442
Cesar Ibarra <ibarratcesar@icloud.com>
2025-01-26 18:08:54 -0800

    Initial Commit

---
+++ Piggy Bank/Piggy_BankApp.swift
@@ -0,0 +1,17 @@
+//
+//  Piggy_BankApp.swift
+//  Piggy Bank
+//
+//  Created by Cesar Ibarra on 1/26/25.
+//
+
+import SwiftUI
+
+@main
+struct Piggy_BankApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
